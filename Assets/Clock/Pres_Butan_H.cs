5ac0212f3762928ed28e91b342c92687ec498e1b
Alex Sutton <alex@Alexs-MacBook-Pro.local>
2016-01-30 23:46:11 -0600

    Adds Clock Scene

---
+++ Assets/Clock/Pres_Butan_H.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pres_Butan_H : MonoBehaviour {
+	// Use this for initialization
+	float maximum_y_position;
+	float minimum_y_position;
+	int moving;
+
+	void Start () {
+	
+		maximum_y_position = this.transform.position.y;
+		minimum_y_position = this.transform.position.y - 0.1f;
+		moving = 0;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton ("Fire1") && moving == 0) {
+			moving = -1;
+		}
+
+		if (this.transform.position.y <= minimum_y_position) {
+			moving = 1;
+			if (clock_updater_a.hours == 11) {
+				if (clock_updater_a.ampm == "AM") {
+					clock_updater_a.ampm = "PM";
+				} else {
+					clock_updater_a.ampm = "AM";
+				}
+			}
+
+			if (clock_updater_a.hours == 12) {
+				clock_updater_a.hours = 1;
+			} else {
+				clock_updater_a.hours += 1;
+			}
+
+		} else if (this.transform.position.y >= this.maximum_y_position && moving != -1) {
+			moving = 0;
+		}
+
+		if (moving > 0) {
+			float new_y = this.transform.position.y + 0.01f;
+			this.transform.position = new Vector3 (transform.position.x, new_y, transform.position.z);
+		} else if (moving < 0) {
+			float new_y = this.transform.position.y - 0.01f;
+			this.transform.position = new Vector3 (transform.position.x, new_y, transform.position.z);
+		}
+	}
+}
---
+++ Assets/Clock/clock_updater_b.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class clock_updater_b : MonoBehaviour {
+	int update_count = 0;
+	public static int seconds = 40;
+	public static int minutes = 59;
+	public static int hours = 11;
+	public static string ampm = "AM";
+
+	int previous_seconds = -1;
+
+	// Use this for initialization
+	void Start () {
+		RenderClock ();
+	}
+
+	string RenderClock() {
+		return PadNumber (hours) + ":" + PadNumber (minutes) + ":" + PadNumber (seconds) + " " + ampm;
+	}
+
+	string PadNumber (int number) {
+		string a = number.ToString();
+		if (a.Length < 2) 
+		{
+			a = "0" + a;
+		}
+		else if (a.Length > 2) 
+		{
+			a = a.Substring(a.Length-2, 2);
+		}
+
+		return(a);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		update_count += 1;
+
+		if (update_count % 60 == 0) {
+			seconds += 1;
+
+			if (seconds == 60) {
+				seconds = 0;
+				minutes += 1;
+
+			}
+
+			if (((hours == 12 && minutes == 0) || (hours == 11 && minutes == 60)) && ( seconds == 0 && previous_seconds == 59)) {
+				if (ampm == "AM"){
+					ampm = "PM";
+				} else {
+					ampm = "AM";
+				}
+			}
+	
+		}
+
+		if (minutes == 60) {
+			minutes = 0;
+			hours += 1;
+		}
+
+		if (hours == 13) {
+			hours = 1;
+
+		}
+		GetComponent<TextMesh> ().text = RenderClock ();
+		previous_seconds = seconds;
+	}
+
+}
